d8962d87a74c5ad699e26773be4ac7c93c4d5588
Adam Gwilliam <adamgwilliam@gmail.com>
2017-09-13 22:44:33 -0500

    account for closed pulls and pagination

--- get_requests.py
+++ get_requests.py
@@ -9,8 +9,17 @@ for i in range(len(json)):
     name = json[i]['name']
     # each value of pulls is a list of pull requests
     # the pull requests are represented as json (dict in python)
-    pulls[name] = requests.get('https://api.github.com/repos/lodash/' + name + '/pulls?access_token=' + OAUTH_TOKEN).json()
-
-
+    url = 'https://api.github.com/repos/lodash/' + name + '/pulls?access_token=' + OAUTH_TOKEN + '&state=all'
+    pulls[name] = [] 
+    while url != '':
+        nextJson = requests.get(url).json()
+        pulls[name].extend(nextJson)
+        header = requests.head(url=url)
+        if ('next' in header.links):
+            url = header.links['next']['url']
+        else:
+            url = ''
+    
 for key in pulls.keys():
     print "repo " + key + " has " + str(len(pulls[key])) + " pull requests"
+
